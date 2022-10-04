7bec6dde0ab22847ab00428a418dcc91dc6789af
gBL17 <fgrodrigues.gabriel@gmail.com>
2022-10-04 11:38:05 -0300

    feat(Animais) create animais service

---
+++ gatitoBook/src/app/animais/animais.service.ts
@@ -0,0 +1,22 @@
+import { environment } from './../../environments/environment';
+import { animais } from './animais';
+import { Observable } from 'rxjs';
+import { TokenService } from './../autenticacao/token.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';]
+
+const API = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimaisService {
+
+  constructor(private http: HttpClient, private tokenService:TokenService) { }
+
+  listaDoUsuario(nomeDoUsuario: string): Observable<animais>{
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<animais>(`${API}/${nomeDoUsuario}/photos`, headers)
+  }
+}
---
+++ gatitoBook/src/app/animais/animais.ts
@@ -0,0 +1,12 @@
+export interface Animal {
+  id: number;
+  postDate: Date;
+  url: string;
+  description: string;
+  allowComments: boolean;
+  likes: number;
+  comments: number;
+  userId: number;
+}
+
+export type animais = Array<Animal>;
